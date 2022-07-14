56540aa3790d4f6adea70e2a330c4eb32117bd2d
LisReyes <lisset.rey2212@gmail.com>
2022-07-14 14:22:14 -0500

    Decision doble con caracteres

--- DecisionDoble/DobleDe.java
+++ DecisionDoble/DobleDe.java
@@ -5,16 +5,34 @@
  * Salida: "Seccion 1" "Seccion 2"
  * LisReyes
  */
-
+import java.util.Scanner;
  class Ejemplo2{
     char letra;
-    public Ejemplo2(){}
+    public Ejemplo2(char y){
+        letra = y;
+    }
 
     public void Validar(){
         if ((letra < 'A') || (letra > 'Z')){
             System.out.println ("F");
         }
-        if 
+        if ((letra >= 'A') && (letra <= 'M')){
+            System.out.println ("Sección 1");
+        }else{
+            System.out.println (" Sección 2");
+        }
+    }
+ }
+
+ class Principal2 {
+    public static void main(String[]args){
+        try(Scanner leer = new Scanner(System.in)){
+            char letra;
+            System.out.println (" Ingrese el caracter a validar ");
+            letra = leer.next().charAt(0);
+            Ejemplo2 ob1 = new Ejemplo2(letra);
+            ob1.Validar();
+        }
     }
  }
 
