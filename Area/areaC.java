90069b24c3be264c87d795839fb789911e9d205d
LisReyes <lisset.rey2212@gmail.com>
2022-07-14 22:00:08 -0500

    Area de un circulo usando la libreria Math.PI

---
+++ Area/areaC.java
@@ -0,0 +1,30 @@
+/* Programa que realice el área de un circulo
+ * Entrada: radio
+ * Salida: Area
+ * LisReyes
+ */
+import java.util.Scanner;
+ class Ejemplo4 {
+    int r;
+    int area;
+    public Ejemplo4 (int y){ r = y;}
+
+    public void AreaC(){
+        if (r > 0){
+            area = (int) (Math.PI * r * r);
+            System.out.println (" El area del circulo es: "+ area);
+        }
+    }
+ }
+
+ class Principal4{
+    public static void main(String[]args){
+        int radio;
+        try (Scanner leer = new Scanner(System.in)) {
+            System.out.println (" Ingrese el valor del radio ");
+            radio = leer.nextInt();
+        }
+        Ejemplo4 ob = new Ejemplo4(radio);
+        ob.AreaC();
+    }
+ }
